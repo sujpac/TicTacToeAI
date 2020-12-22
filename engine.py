eaad51c535126e6c172b5a05354083ac91cc6dd8
sujpac <sujit.pack@gmail.com>
2020-12-21 20:41:11 -0800

    Finished basic game

--- engine.py
+++ engine.py
@@ -1,8 +1,10 @@
+from ast import literal_eval
+
 BOARD_WIDTH = 3
 BOARD_HEIGHT = 3
 
 def new_board():
-    board = [[None] * BOARD_WIDTH for _ in range(BOARD_HEIGHT)]
+    board = [['-'] * BOARD_WIDTH for _ in range(BOARD_HEIGHT)]
     return board
 
 def render(board):
@@ -10,13 +12,67 @@ def render(board):
         print(' '.join(row))
 
 def get_move():
-    pass
+    tup_string = input('Enter Move: ')
+    return literal_eval(tup_string)
 
-def make_move():
-    pass
+def make_move(board, move, player):
+    board[move[0]][move[1]] = player
+    # TODO: add logic for invalid moves
+    return True
 
-def get_winner():
-    pass
+def get_winner(board):
+    lines = []
+    # Add horizontal lines
+    for row in range(BOARD_HEIGHT):
+        lines.append([])
+        for col in range(BOARD_WIDTH):
+            lines[-1].append(board[row][col])
+    # Add vertical lines
+    for col in range(BOARD_WIDTH):
+        lines.append([])
+        for row in range(BOARD_HEIGHT):
+            lines[-1].append(board[row][col])
+    # Add diagonal lines
+    # TODO: diagonal horizontal lines independent of board size
+    lines.append([board[0][0], board[1][1], board[2][2]])
+    lines.append([board[0][2], board[1][1], board[2][0]])
 
-def is_board_full():
-    pass
+    for line in lines:
+        if len(set(line)) == 1 and line[0] != '-':
+            return line[0]
+    return None
+
+def is_board_full(board):
+    for i in range(BOARD_HEIGHT):
+        for j in range(BOARD_WIDTH):
+            if board[i][j] == '-':
+                return False
+
+    return True
+
+def play():
+    players = [
+        'X',
+        'O'
+    ]
+    turn_number = 0
+    board = new_board()
+
+    while True:
+        current_player = players[turn_number % 2]
+        render(board)
+
+        move_coords = get_move()
+        make_move(board, move_coords, current_player)
+        winner = get_winner(board)
+
+        if winner:
+            print('Winner is %s' % winner)
+            render(board)
+            break
+
+        if is_board_full(board):
+            print("It's a draw!")
+            break
+
+        turn_number += 1
