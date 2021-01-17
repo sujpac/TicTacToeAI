fed510ef0e59be3a4712519d525a0e762bc735e0
sujpac <sujit.pack@gmail.com>
2021-01-17 12:39:32 -0800

    Add cached implementation of minimax algo

--- minimax_ai.py
+++ minimax_ai.py
@@ -10,7 +10,7 @@ def minimax_ai(board, player):
     for move in legal_moves:
         _board = copy.deepcopy(board)
         engine.make_move(_board, move, player)
-        score = _minimax_score(_board, opponent, player)
+        score = _cached_minimax_score(_board, opponent, player)
         scores.append(score)
 
     sorted_best_moves = [x for _, x in sorted(zip(scores, legal_moves))]
@@ -36,6 +36,40 @@ def _minimax_score(board, player_current, player_main):
         score = _minimax_score(_board, opponent, player_main)
         scores.append(score)
 
+    if player_current == player_main:
+        return max(scores)
+    else:
+        return min(scores)
+
+cache = {}
+
+def _cached_minimax_score(board, player_current, player_main):
+    winner = engine.get_winner(board)
+    opponent = utils.get_opponent(player_current)
+
+    if winner == player_main:
+        return 10
+    elif winner == opponent:
+        return -10
+    elif engine.is_board_full(board):
+        return 0
+
+    board_cache_key = str(board)
+    is_main_player = player_current == player_main
+    if board_cache_key in cache:
+        scores = cache[board_cache_key]
+        return max(scores) if is_main_player else min(scores)
+
+    legal_moves = utils.get_legal_moves(board, player_current)
+    scores = []
+
+    for move in legal_moves:
+        _board = copy.deepcopy(board)
+        engine.make_move(_board, move, player_current)
+        score = _cached_minimax_score(_board, opponent, player_main)
+        scores.append(score)
+
+    cache[board_cache_key] = scores
     if player_current == player_main:
         return max(scores)
     else:
